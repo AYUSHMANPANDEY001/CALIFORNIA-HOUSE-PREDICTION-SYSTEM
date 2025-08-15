37c768daf6f436a9c8607550145715acfdb78b4d
AYUSHMANPANDEY001 <pandeyayushman01@gmail.com>
2025-08-15 12:50:10 +0530

    added the project

---
+++ loading.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug  9 19:43:57 2025
+
+@author: AYUSHMAN PANDEY
+"""
+
+import numpy as np
+import pickle
+
+#Loading the saved model
+loaded_model = pickle.load(open('C:/Users\AYUSHMAN PANDEY\Desktop\ML PROJECT DEPLOYMENT\model.pkl', 'rb'))
